bbe75d024fa0e5253bae531004041190e36e6a7e
thebkbuffalo <evan.m.berg@gmail.com>
2014-09-23 15:49:57 -0400

    FUCK FUCK

--- config/boot.rb
+++ config/boot.rb
@@ -9,12 +9,9 @@ DB = Sequel.connect("postgres://localhost:5432/dvr_app_development")
 
 Dir['./helpers/*.rb'].each { |helper| require helper }
 Dir['./models/*.rb'].each { |model| require model }
-<<<<<<< HEAD
-require 'rack-flash'
-require './controllers/application_controller'
-=======
+
 
 require './controllers/application_controller'
 
->>>>>>> d82351cbbb382d758659fe3309f600dccc4babfe
+
 Dir['./controllers/*.rb'].each { |controller| require controller }
